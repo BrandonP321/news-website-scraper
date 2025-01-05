d51233e602ba5f1d015984e0468fc79175b55da5
Brandon Phillips <brandon.phillips@bphillips.dev>
2025-01-05 14:05:27 -0800

    Add chat gpt link

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import styles from "./App.module.scss";
 import { ArticleScraper } from "./ArticleScraper";
 import { Button } from "react-bootstrap";
 
+const customGPTUrl =
+  "https://chatgpt.com/g/g-677a34722478819182415ab276a656f0-intelligence-briefing-assistant";
+
 function App() {
   const [scrapersCount, setScrapersCount] = useState(1);
 
@@ -11,6 +14,9 @@ function App() {
   return (
     <div className={styles.app}>
       <div className={styles.btnWrapper}>
+        <Button href={customGPTUrl} target="_blank" className={styles.leftBtn}>
+          Go to ChatGPT
+        </Button>
         <Button onClick={() => setScrapersCount(scrapersCount + 1)}>
           Add Article Scraper
         </Button>
